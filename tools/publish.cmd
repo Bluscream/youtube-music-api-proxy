1d353eb2f2ac888e31e87448508c2104f636511b
Bluscream <Bluscream@users.noreply.github.com>
2025-08-04 07:02:49 +0200

    Build at 2025-08-04 07:02:49

--- tools/publish.cmd
+++ tools/publish.cmd
@@ -1,2 +1,4 @@
 @echo off
+set GITHUB_USERNAME=Bluscream
+set DOCKER_USERNAME=Bluscream1
 powershell -ExecutionPolicy Bypass -Command "publish.ps1 -Git -Github -Docker -Ghcr -Repo \"youtube-music-api-proxy\""
